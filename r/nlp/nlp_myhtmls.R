05c031fea62b707193771890edd8f43682c03bf1
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2024-03-26 13:26:45 +0530

    a

--- r/nlp/nlp_myhtmls.R
+++ r/nlp/nlp_myhtmls.R
@@ -3,21 +3,41 @@ pacman::p_load(tidyverse, rvest)
 
 # Start by reading a HTML page with read_html():
 urllocal = 'Users/du/dup/analytics/projects/mypractise/data/student2.html'
-urlLink =  'https://raw.githubusercontent.com/dupadhyaya/mypractise/main/data/student2.html'
+urlLink2 =  'https://raw.githubusercontent.com/dupadhyaya/mypractise/main/data/student2.html'
+urlLink3 =  'https://raw.githubusercontent.com/dupadhyaya/mypractise/main/data/student3.html'
+#show as webpage
+#https://html-preview.github.io/?url=https://github.com/dupadhyaya/mypractise/blob/main/data/student3.html
 
-sthtml <- read_html(urlLink)
-sthtml
+sthtml2 <- read_html(urlLink2)
+sthtml2
 
 # Then find elements that match a css selector or XPath expression
-# using html_elements(). In this example, each <section> corresponds
-# to a different film
-sthtml %>% html_elements('h1') %>% html_text2()
-sthtml %>% html_elements('table') %>% html_text2()
+# using html_elements().
+sthtml2 %>% html_elements('h1') %>% html_text2()
+sthtml2 %>% html_elements('table') %>% html_text2()
 
 #select table using css selector
-sthtml %>% html_nodes('table') %>% .[[1]] 
-sthtml %>% html_nodes('table') %>% .[[1]] %>% html_table()
+sthtml2 %>% html_nodes('table') %>% .[[1]] 
+sthtml2 %>% html_nodes('table') %>% .[[1]] %>% html_table()
 
+#student3-----
+sthtml3 <- read_html(urlLink3)
+sthtml3
+sthtml3 %>% html_elements('h1') %>% html_text2()
+sthtml3 %>% html_elements('table') %>% html_text2()
+sthtml3 %>%  html_element("p") %>% html_text()
 
-title <- films %>% html_element("h2") %>%  html_text2()
-title
+#select table using css selector
+sthtml3 %>% html_nodes('table') %>% .[[1]] 
+sthtml3 %>% html_nodes('table') %>% .[[1]] %>% html_table()
+sthtml3 %>% html_nodes('table') %>% .[[1]] 
+sthtml3 %>% html_nodes(xpath="//table[@class='tbl']") %>% html_table()
+#sthtml3 %>% html_nodes(xpath="//*[@class='tbl']/table[1]") %>% html_table()
+#//p | //th+//th
+sthtml3 %>% html_nodes(xpath="//table") %>% html_table()
+
+#usingSelenium-----
+#library(selenium)
+pacman::p_load(RSelenium, purrr, rvest, glue)
+#https://www.java.com/en/download/
+system('java -version')
---
+++ r/nlp/selenium1.R
@@ -0,0 +1,3 @@
+# Selenium in R
+
+library(selenium)
